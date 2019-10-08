bd4805e7f46f586313dae550a5ee7bfcff936058
Matthew Wu <wumatthew2018@gmail.com>
2019-10-08 21:04:01 +0900

    updated

---
+++ japan2019/script.js
@@ -0,0 +1,55 @@
+document.getElementById("day-3-button").addEventListener("click", function(){
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  window.location.href="#d3";
+});
+
+document.getElementById("day-4-button").addEventListener("click", function(){
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  window.location.href="#d4";
+});
+
+document.getElementById("day-5-button").addEventListener("click", function(){
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  window.location.href="#d5";
+});
+
+document.getElementById("day-6-button").addEventListener("click", function(){
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  window.location.href="#d6";
+});
+
+document.getElementById("day-7-button").addEventListener("click", function(){
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  window.location.href="#d7";
+});
+
+document.getElementById("day-8-button").addEventListener("click", function(){
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  window.location.href="#d8";
+});
+
+document.getElementById("day-9-button").addEventListener("click", function(){
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  window.location.href="#d9";
+});
+
+document.getElementById("day-10-button").addEventListener("click", function(){
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  window.location.href="#d10";
+});
